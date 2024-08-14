65680824cca246f1d9b736bfd21934d2f0007ea4
Vanessa Lai <vlaiyz2605@gmail.com>
2024-08-14 21:59:05 +0800

    added live

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = 'https://ultimate-ttt-wk1e.onrender.com';
 
 export const socket = io(SOCKET_URL, {
   transports: ['websocket'],
